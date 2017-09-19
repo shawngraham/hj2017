b523eecdba7de7e0ffea151d037e5080d085d38f
Shawn Graham <shawn_graham@carleton.ca>
2017-09-19 09:19:01 -0400

    Add files via upload

---
+++ newsgrammar.js
@@ -0,0 +1,45 @@
+/**
+ * @author Shawn
+ */
+
+var grammar = {
+
+	"name": [
+		"Bertram",
+		"Arabella",
+		"Cecil"
+	],
+	"occupation": [
+		"baker",
+		"wizard",
+		"soldier"
+	],
+
+	"origin": [
+		"#name# the #occupation#","#occupation# the #name#", "#occupation.s# are rioting","#oldstories#"],
+
+	"headline": ["#name# in the #occupation# scandal!","'Kill the #occupation#!' says #name#"],
+
+	"kittenPhotoURL": [
+		"http://media.tumblr.com/tumblr_laxjtlso9K1qzjix8.gif"
+	],
+	"reactionURL": [
+		"http://tinyurl.com/nf8t58q",
+		"http://tinyurl.com/p7lhd4r",
+		"http://tinyurl.com/p8jzq4g",
+		"http://tinyurl.com/lt42vrx",
+		"http://tinyurl.com/3ghat59",
+		"http://tinyurl.com/mx4yqk9",
+		"http://tinyurl.com/lssxt9v",
+		"http://tinyurl.com/nf6oy85",
+		"https://i.giphy.com/media/h5NLPVn3rg0Rq/giphy.webp",
+		"/imgs/PW7MoTD2d9pJK.gif"
+	],
+	"kittenPhoto": [
+		"<br><img class='media' src='#reactionURL#' width='220'><br>"
+	],
+	"kittenorigin": [
+		"#kittenPhoto#<figcaption class='figcaption'>How computer scientists see IF authors</figcaption>","#kittenPhoto#<figcaption class='figcaption'>How IF authors see computer scientists</figcaption>","#kittenPhoto#<figcaption class='figcaption'>How the rest of the world sees both of them</figcaption>","",""
+	],
+	"oldstories": ["Mr. John Bums of Admaston was out hunting recently on the Opeongo with his brother. At a certain point they seperated to take different roads which would eventually bring them together again. John however changed his mind and taking another road fell into his brother's backs. He mounted a log summit - what icy to discover the whereabouts of bis brother and a sudden noise behind him caused him to turn sharply. He slipped on the log his hand lowing the gun which was UiseluugeU by a projection of the log; and the bullet went through his left twice entering the under side of the arm near the wrist and coming out finally underneath just below the elbow. A number of small bones were broken;  Mr Burns was under surgical treatment in Renfrew for a day or so. He had a narrow escape from more  serious injuries."]
+}
